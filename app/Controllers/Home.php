2c9db089c7df00b8d83a3c01f748e4503d26ca97
subejoonfire <harlanmuradi@gmail.com>
2025-03-27 05:51:06 +0800

    dashboard karyawan counter updated

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,13 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Models\admin\KaryawanModel;
+
 class Home extends BaseController
 {
+    public $karyawanModel;
+    public function __construct()
+    {
+        $this->karyawanModel = new KaryawanModel();
+    }
     public function index()
     {
+        $data = [
+            'jumlahKaryawan' => $this->karyawanModel->countAll(),
+        ];
         echo view('temp/header');
         echo view('temp/navbar');
-        echo view('admin/dashboard');
+        echo view('admin/dashboard', $data);
     }
-
 }
